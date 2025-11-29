128b6c780cef61e56985b7a2a04ef71a70962dcc
dandxy89 <dan@functorml.co.uk>
2025-11-29 14:09:50 +0000

    feat: Handle whitespace
    https://docs.rs/csv/1.4.0/csv/struct.ReaderBuilder.html#method.trim

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,15 +1,19 @@
-use std::io;
-use std::io::Write;
+use std::io::{BufReader, Write};
+use std::{fs::File, io};
 
 use silhouette::{file_reader::csv_stream, ledger::engine::PaymentsEngine};
 
 fn main() {
     let file_path = std::env::args().nth(1).expect("No file_path was provided");
+    let Ok(file) = File::open(&file_path) else {
+        panic!("Failed to open file at path: {file_path}");
+    };
+    let buffer = BufReader::new(file);
 
     let mut payment_engine = PaymentsEngine::default();
     let mut stderr = io::stderr().lock();
 
-    for csv_record in csv_stream(&file_path) {
+    for csv_record in csv_stream(buffer) {
         if let Ok(record) = csv_record {
             if let Err(err) = payment_engine.process_csv_record(record) {
                 let _ = writeln!(stderr, "Error processing Transaction due to {err:?}");
--- src/file_reader.rs
+++ src/file_reader.rs
@@ -1,12 +1,32 @@
-use std::fs::File;
+use std::io;
 
 use crate::model::CSVRecord;
 
-/// TODO: Whitespace and decimal precision up to 4dps
-pub fn csv_stream(path: &str) -> impl Iterator<Item = Result<CSVRecord, csv::Error>> {
-    let file = File::open(path).unwrap();
+pub fn csv_stream<R: io::Read>(buffer: R) -> impl Iterator<Item = Result<CSVRecord, csv::Error>> {
     let reader = csv::ReaderBuilder::new()
         .has_headers(true)
-        .from_reader(file);
+        .trim(csv::Trim::All) // Strip Whitespace
+        .from_reader(buffer);
+
     reader.into_deserialize::<CSVRecord>()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::model::TxType;
+
+    #[test]
+    fn trimming_test() {
+        let test_data = r#" type,  client,  tx,  amount
+deposit,  1,  1,  100.0
+"#;
+
+        let mut reader = super::csv_stream(test_data.as_bytes());
+
+        let record = reader.next().unwrap().unwrap();
+        assert_eq!(record.r#type, TxType::Deposit);
+        assert_eq!(record.client, 1);
+        assert_eq!(record.tx, 1);
+        assert!(record.amount.is_some());
+    }
 }
